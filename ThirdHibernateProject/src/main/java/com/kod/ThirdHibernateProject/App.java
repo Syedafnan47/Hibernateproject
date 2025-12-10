3853f4cf9b1731355584eca19c2cfb7d7539dddc
Syedafnan47 <syedtarooq786@gmail.com>
2025-12-10 12:35:30 +0530

    third

---
+++ ThirdHibernateProject/src/main/java/com/kod/ThirdHibernateProject/App.java
@@ -0,0 +1,52 @@
+package com.kod.ThirdHibernateProject;
+
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    { 
+    	Scanner in=new Scanner(System.in);
+    	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory(); 
+    	//above i have done in 1 step
+    	
+    	Session session= factory.openSession();
+    	
+    	Transaction tx = session.beginTransaction();
+    	
+        System.out.println( "Enter Student id to update");
+        int id=in.nextInt();
+        in.nextLine();
+       Student obj =  session.get(Student.class, id);
+       
+       if(obj!=null) {
+    	   System.out.println("Enter name and email to update");
+    	   String name=in.nextLine();
+    	   String email=in.nextLine();
+    	   
+    	   obj.setName(name);
+    	   obj.setEmail(email);
+    	   
+    	   session.merge(obj);
+    	   System.out.println("Student "+id+" Updated successfully");
+       }
+       else {
+    	   System.out.println("Student with id "+id+" not found to update");
+       }
+       
+       tx.commit();
+       
+       session.close();
+       factory.close();
+        
+    }
+}
